d73eab866ce34bb560acc1da22ad07c9cc017c3d
Lewis Watson <lewiswatson55@protonmail.com>
2020-06-08 22:18:04 +0100

    Added Code Comments

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+//Include required modules and configuration files
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const packageInfo = require("./package.json");
 
+//
 const bot = new Discord.Client()
 
-bot.on("ready", async () => {
-  const user = bot.users.cache.get(botconfig.userid);
-  await user.send(process.argv[2]);
-  process.exit(0);
+bot.on("ready", async () => { //when bot ready
+  const user = bot.users.cache.get(botconfig.userid); //Identify user to message
+  await user.send(process.argv[2]); //Wait for message to send to user
+  process.exit(0); //End program with success code
 })
 
-bot.login(botconfig.token);
+bot.login(botconfig.token); //Log into discord bot using private token.
